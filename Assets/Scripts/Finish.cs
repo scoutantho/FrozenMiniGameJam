4594191caf765ed717e6ad785a8a04462cb9c0b0
anthony.gregori78200@gmail.com <anthony.gregori78200@gmail.com>
2023-11-11 19:19:32 -0500

    Adding finish mine animations adding door frame handling

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -7,11 +7,28 @@ using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+    private Animator animator;
     private bool IsEntranceStillStanding = true;
+    private bool IsInDoorFrame = false;
+    private bool NextLevelLoading = false;
     // Start is called before the first frame update
     void Start()
     {
         finishSound = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        Debug.Log($"Entrance is still standing : {IsEntranceStillStanding} and player is in door frame : {IsInDoorFrame}");
+        if (IsInDoorFrame && IsEntranceStillStanding && !NextLevelLoading)
+        {
+            if (Input.GetAxisRaw("Vertical") >= 0.1f)
+            {
+                //finishSound.Play();
+                NextLevelLoading = true;
+                Invoke("CompleteLevel", 0.5f);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,26 +38,23 @@ public class Finish : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftControl))
             {
                 IsEntranceStillStanding = false;
-                Debug.Log("Destroy Entrance");
+                animator.SetTrigger("IsDestroyed");
             }
+            IsInDoorFrame = true;
         }
     }
-
-    private void OnTriggerStay2D (Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log($"Entrance is : {IsEntranceStillStanding}");
-        if (collision.gameObject.CompareTag("Player") && IsEntranceStillStanding)
+        Debug.Log("^Player leave door frame");
+        if (collision.gameObject.CompareTag("Player"))
         {
-            if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                //finishSound.Play();
-                Invoke("CompleteLevel", 0.5f);
-            }
+            IsInDoorFrame = false;
         }
     }
 
     private void CompleteLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        NextLevelLoading = false;
     }
 }
