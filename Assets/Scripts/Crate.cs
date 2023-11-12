e0a840065a17175f1434cf7d16c839ab64e5b45e
anthony.gregori78200@gmail.com <anthony.gregori78200@gmail.com>
2023-11-11 20:02:01 -0500

    Adding crate

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    private Animator animator;
+    private bool IsCrateStillStanding = true;
+    private bool IsInFrame = false;
+    private bool IsAlreadyLooted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsInFrame && IsCrateStillStanding && !IsAlreadyLooted)
+        {
+            if (Input.GetAxisRaw("Vertical") >= 0.1f)
+            {
+                //finishSound.Play();
+                IsAlreadyLooted = true;
+                Invoke("OptainSword", 0.1f);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && IsCrateStillStanding)
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                IsCrateStillStanding = false;
+                animator.SetTrigger("IsDestroyed");
+            }
+            IsInFrame = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Player leave door frame");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsInFrame = false;
+        }
+    }
+
+    private void OptainSword()
+    {
+        Debug.Log("You obtain a sword");
+    }
+
+}
